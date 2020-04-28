c32ad6fecda7394cbe9ad20096aacf52ec957820
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-04-28 19:47:43 +0900

    Change: main.shを適応法の移動音の実験に対応

--- make_testsignal/main.sh
+++ make_testsignal/main.sh
@@ -7,7 +7,7 @@
 # Ryota Shimokawara (sr17805@tomakomai.kosen-ac.jp)
 # 2018
 # ######################################################################
-# sh main (subject_directory : ex. hogehoge/SHIMIZU) (LSTF_directory : hogehoge/LSTF)
+# bash main (subject_directory : ex. ../SUBJECTS/TETSU) (LSTF_directory : ../LSTF)
 # ######################################################################
 
 # ファイルの上書き防止 && エラーが起きたら停止 && 変数の空文字列防止
@@ -19,13 +19,15 @@ if [ $# -ne 2 ]; then
   exit
 fi
 
-mkdir -p $1/TS $1/ANSWER
+SUBJECT_DIR=$1
+LSTF_DIR=$2
 
-sh make_SLTF.sh $1 $2 # HRTFとSLTFの生成
-sh make_SLTF.sh $1 # HRTFとSLTFの生成
-sh liner_interpolation.sh $1 # SLTFの線形補間
-sh make_stationary_sound_images $1 # 静止音像の作成
-sh make_moving_sound_images.sh $1 # 移動音像の作成
-sh direction_confirm_sound.sh $1 # 方向確認用の静止音像の作成
+mkdir -p ${SUBJECT_DIR}/TS ${SUBJECT_DIR}/ANSWER
+
+bash make_SLTF.sh ${SUBJECT_DIR} ${LSTF_DIR} # HRTFとSLTFの生成
+bash liner_interpolation.sh ${SUBJECT_DIR} # SLTFの線形補間
+bash make_testsignal_move_judge0_msdv.sh ${SUBJECT_DIR} # 移動音の生成
+bash make_testsignal_move_judge45_msdv.sh ${SUBJECT_DIR} # 移動音の生成
+bash make_testsignal_move_judge90_msdv.sh ${SUBJECT_DIR} # 移動音の生成
 
 echo "completed!!"
