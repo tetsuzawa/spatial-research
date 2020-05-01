8c345ac6e344c350cbadc0b5c56c820b5e650e07
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-05-01 14:15:53 +0900

    Add: beta

--- make_testsignal/best_PEST.py
+++ make_testsignal/best_PEST.py
@@ -30,6 +30,8 @@ class BestPEST:
         回答数 = 試行回数
     C: int
         正解数
+    Y: float
+        正答率 = C/T
     M: float
         曲線の中点。最尤法によってXをMに近づけていく。
     S: float
@@ -45,8 +47,10 @@ class BestPEST:
         self.likelihood = 0
         self.T = 0
         self.C = 0
-        self.M = 0.0
-        self.S = 1.0
+        self.Y = 0
+        self.M = 40
+        self.S = 10
+        self._B = np.array([-self.M / self.S, 1 / self.S], dtype=float)
         self.gradient_vec = np.zeros(shape=2, dtype=np.float)
         self.hessian_mat = np.zeros(shape=(2, 2), dtype=np.float)
 
@@ -95,8 +99,8 @@ class BestPEST:
 
         Returns
         -------
-        PF(X; M,S): float
-            心理測定関数の計算結果
+        X: float
+            刺激レベル（移動角度や移動速度など）
         """
 
         pf_inv = M + 2 * S * np.log(Y)
@@ -182,6 +186,44 @@ class BestPEST:
         t3 = (self.T - self.C) * -self._W_SS(X)
         return t1 + t2 + t3
 
+    def _L_B0(self) -> float:
+        """
+        B0によるLの偏微分
+        """
+        return - (self.likelihood - self.Y)
+
+    def _L_B1(self, X: float) -> float:
+        """
+        B1によるLの偏微分
+        """
+        return - (self.likelihood - self.Y) * X
+
+    def _L_B0B0(self) -> float:
+        """
+        B0によるLの2階偏微分
+        """
+        return self.likelihood * (1 - self.likelihood)
+
+    def _L_B0B1(self, X: float) -> float:
+        """
+        B0とB1によるLの2階偏微分
+        L_B1B0と等しい
+        """
+        return self.likelihood * (1 - self.likelihood) * X
+
+    def _L_B1B0(self, X: float) -> float:
+        """
+        B1とB0によるLの2階偏微分
+        L_B0B1と等しい
+        """
+        return self._L_B0B1(X)
+
+    def _L_B1B1(self, X: float) -> float:
+        """
+        B1によるLの2階偏微分
+        """
+        return self.likelihood * (1 - self.likelihood) * X * X
+
     def update(self, is_correct: bool, X: float):
         """
         Best-PEST法のパラメータを更新する関数
@@ -197,9 +239,67 @@ class BestPEST:
         self.T += 1
         if is_correct:
             self.C += 1
-        self.likelihood = self.PF(X, self.M, self.C)
-        self.gradient_vec += np.array([self._L_M(), self._L_S(X)])
-        self.hessian_mat += np.array([[self._L_MM(), self._L_MS(X)],
-                                      [self._L_MS(X), self._L_SS(X)]])
-        u = np.array([self.M, self.S]).T - np.linalg.inv(self.hessian_mat) @ self.gradient_vec.T
-        self.M, self.S = u
+        self.Y = self.C / self.T
+        for _ in range(100):
+            self.likelihood = self.PF(X, self.M, self.S)
+            # self.gradient_vec += np.array([self._L_M(), self._L_S(X)])
+            # self.hessian_mat += np.array([[self._L_MM(), self._L_MS(X)],
+            #                               [self._L_MS(X), self._L_SS(X)]])
+            self.gradient_vec = np.array([self._L_B0(), self._L_B1(X)])
+            self.hessian_mat = np.array([[self._L_B0B0(), self._L_B0B1(X)],
+                                         [self._L_B1B0(X), self._L_B1B1(X)]])
+            dB = np.linalg.pinv(self.hessian_mat) @ self.gradient_vec.T
+            # print("B:", self._B)
+            self._B += dB
+            # print("B:", self._B)
+            # print(f"MMMM:{self.M}, SSSS:{self.S}")
+            self.M = - self._B[0] / self._B[1]
+            self.S = 1 / self._B[1]
+            print(f"MMMM:{self.M}, SSSS:{self.S}")
+        # Debug
+        # print("X:", X)
+        # print("C:", self.C)
+        # print("T:", self.T)
+        # print("Y:", self.Y)
+        # print("likelihood:", self.likelihood)
+        # print("B:", self._B)
+        # print("gradient_vec:", self.gradient_vec)
+        # print("hessan_mat:", self.hessian_mat)
+        # print("dB:", dB)
+        # print("plused B:", self._B)
+        # print("\n\n")
+
+# パーセプトロンは無理（入力がM, S）になってるから。本当はXからM，Sを推定しないといけない
+# def update_perceptron(self, is_correct: bool, X: float, M: float, S: float):
+#     # data = np.loadtxt('logistic2.csv', delimiter=',', skiprows=1)  # 学習データ読み込み
+#     # x = data[:, 0:2]  # x1,x2データ
+#     # t = data[:, 2]  # 分類を示すラベル
+#     self.x = np.append(self.x, [[X / S, -M / S]], axis=0)
+#     if is_correct:
+#         np.append(self.t, [[1]], axis=0)
+#     else:
+#         np.append(self.t, [[0]], axis=0)
+#     stdized_x = (self.x - self.x.mean(axis=0)) / self.x.std(axis=0)  # 標準正規化
+#     x0 = np.ones([stdized_x.shape[0], 1])  # バイアス項作成
+#     # x3 = stdized_x[:, 0, np.newaxis] ** 2  # x1を2乗した項を作成
+#
+#     # matrix_x = np.hstack([x0, stdized_x, x3])  # 1, x1, x2, x1^2の順
+#     matrix_x = np.hstack([x0, stdized_x])  # 1, X/S, -M/S,
+#     w = np.random.rand(4)  # 重み初期化
+#     rate = 0.01  # 学習率
+#     loop = 200000  # 更新回数
+#
+#     # 重み更新
+#     for i in range(loop):
+#         sig = 1 / (1 + np.exp(-np.dot(matrix_x, w)))  # シグモイド関数
+#         w += rate * np.dot((t - sig) * sig * (1 - sig), matrix_x)  # 重み更新
+#
+#     print(round(-w[0] / w[2], 2), ',', round(-w[1] / w[2], 2), ',', round(-w[3] / w[2], 2))  # 重み最終値
+#
+#     # 描画
+#     x_axis = np.linspace(start=-2, stop=1.5, num=100)
+#     plt.plot(stdized_x[t == 1, 0], stdized_x[t == 1, 1], '^')  # ラベルが1のデータプロット
+#     plt.plot(stdized_x[t != 1, 0], stdized_x[t != 1, 1], 'o')  # ラベルが1以外のデータプロット
+#     plt.plot(x_axis, -(w[0] + w[1] * x_axis + w[3] * x_axis ** 2) / w[2])  # 境界線プロット
+#     plt.grid(True)
+#     plt.show()
--- make_testsignal/test_best_PEST.py
+++ make_testsignal/test_best_PEST.py
@@ -13,12 +13,12 @@ def main():
     bp = BestPEST()
 
     M = 20
-    S = 1
-    r = range(1, 50)
+    S = 3
+    r = range(1, 51)
     mock_x = list(r)
     mock_y = list(map(BestPEST.PF, map(float, r), np.full(50, M), np.full(50, S)))
 
-    for i in r:
+    for i in range(1, 100):
         if i == 1:
             X = 50
         elif i == 2:
@@ -26,21 +26,33 @@ def main():
         else:
             X = bp.M
 
-        print(f"M:{bp.M}, S:{bp.S}")
+        if X < 1:
+            X = 1
+        elif 50 < X:
+            X = 50
+
         is_correct = np.random.rand() < BestPEST.PF(X, M, S)
+        # is_correct = BestPEST.PF(X, bp.M, bp.S) < BestPEST.PF(X, M, S)
+        print(f"i:{i},\t X:{X:.3f},\t Y:{BestPEST.PF(X, bp.M, bp.S):.3f},\t mockY:{BestPEST.PF(X, M,S):.3f},\t M:{bp.M:.3f},\t S:{bp.S:.3f},\t is_correct:{is_correct}")
         bp.update(is_correct, X)
+        mock_tmp_y = list(map(BestPEST.PF, map(float, r), np.full(50, bp.M), np.full(50, bp.S)))
+        plt.plot(mock_x, mock_tmp_y)
 
     Y = 0.75
     print(f"mockの閾値 y=0.75に対するxの値は x={BestPEST.PF_inv(Y, M, S)}")
     print(f"Best-PESTの閾値 y=0.75に対するxの値は x={BestPEST.PF_inv(Y, bp.M, bp.S)}")
-    print(f"Best-PESTの回答数: {bp.T}, 正答数: {bp.C}, 正答率: {bp.C / bp.T*100:3.1f}%")
+    print(f"Best-PESTの回答数: {bp.T}, 正答数: {bp.C}, 正答率: {bp.C / bp.T * 100:3.1f}%")
 
+    # m31s7_8_y = list(map(BestPEST.PF, map(float, r), np.full(50, 31.4), np.full(50, 7.85)))
+    # m40s7_10_y = list(map(BestPEST.PF, map(float, r), np.full(50, 40), np.full(50, 10)))
     y = list(map(BestPEST.PF, map(float, r), np.full(50, bp.M), np.full(50, bp.S)))
 
     plt.plot(mock_x, mock_y, "r--")
+    # plt.plot(mock_x, m31s7_8_y, "y--")
+    # plt.plot(mock_x, m40s7_10_y, "g--")
     plt.plot(mock_x, y, "b--")
     plt.show()
 
 
 if __name__ == '__main__':
-    main()
+    main1()
