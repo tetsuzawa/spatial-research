9a7128430068960abce7faad8c7135f4720afba0
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-05-06 13:31:20 +0900

    Update: アルゴリズムを修正し、exampleを追加した

--- continuous_move_judge/PEST.py
+++ continuous_move_judge/PEST.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # ##################################################
@@ -9,32 +9,74 @@
 # ##################################################
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class PEST:
+    """PEST法による実験のためのクラス
+
+    PEST法 (Parameter Estimation by Sequential Testing) は心理物理測定法のうちの適応法の一つである.
+    (a)刺激レベルを変える時期と, (b)刺激レベルの変化規則によって刺激レベルを逐次的に変化させて実験を行う.
+
+    参考:
+        津村尚志 "最近の聴覚心理実験における新しい測定法"（1984）
+        津村尚志 "聴覚実験における計量心理学の応用"（1986）
+        津村尚志 "聴覚心理測定法の最近の動向"（1991）
+        寺岡章人, 津村尚志 "訓練を受けた聴取者と受けていない聴取者を用いた三つの心理物理測定法の実験効率の比較"（1998）
+
+    :cvar _lower: 刺激レベルの変化方向（難化方向）.
+    :cvar _upper: 刺激レベルの変化方向（易化方向）.
+
+    :ivar Xt: 刺激レベルの目標値Xt. 閾値. これを求める.
+    :ivar Pt: 刺激レベルの目標値Ltに対する特定応答の出現率. 閾値. 強制選択法（2IFC）なら0.75, Yes/No法なら0.5.
+    :ivar dx: 刺激変化幅.
+    :ivar _min_dx: 最小刺激変化幅. 刺激幅dxが最小刺激幅min_dxとなったときに実験を終了.
+    :ivar _max_dx: 最大刺激変化幅. 被験者の混乱を避けるため定義.
+    :ivar _doubled_dx: 刺激変化幅が2倍されたものかの情報を保持.
+    :ivar _switched_direction_by_doubled_dx: 刺激変化方向がが2倍された刺激変化幅によるものか情報を保持.
+    :ivar _pre_stimulation_level_direction: # 前回の刺激レベルの変化方向の情報を保持.
+    :ivar _repeated_num: 連続で同じ回答が繰り返された回数. 最小値:1.
+    :ivar _consecutive_T: 一定の刺激レベルXで繰り返された回答数.
+    :ivar _consecutive_C: 一定の刺激レベルXで繰り返された正答数.
+    :ivar _W:  W  # deviation limit. 1.0 <= W <= 2.0.
+    """
     _lower = False
     _upper = True
 
-    def __init__(self, initial_dx=16, min_dx=1, Pt=0.75, W=1.0):
-        self.pre_stimulation_level_direction = self._lower  # lower:False or upper:True
-        self.repeated_num = 1  # 連続で同じ回答が繰り返された回数。最小値:1,
-        self.dx = initial_dx  # 刺激変化幅。
-        self.min_dx = min_dx  # 最小刺激変化幅。刺激幅dxが最小刺激幅min_dxとなったときに実験を終了する。
-        self.doubled_dx = False
-        self.switched_direction_by_doubled_dx = False
-        self.T = 0  # 回答数
-        self.C = 0  # 正答数
-        self.W = W  # deviation limit. 1.0 <= W <= 2.0
-        self.Xt = 0  # 刺激レベルの目標値Xt。これを求める。
-        self.Pt = Pt  # 刺激レベルの目標値Ltに対する特定応答の出現率。閾値。強制選択法（2IFC）なら0.75, Yes/No法なら0.5。
+    def __init__(self, init_dx=16, min_dx=1, Pt=0.75, W=1.0):
+        """初期化関数
+
+        :param init_dx: 刺激レベルの初期値. 最小刺激レベルの2^4以上が望ましい.
+        :param min_dx: 最小刺激レベル. 変化幅が最小刺激レベルとなったときに実験を終了する.
+        :param Pt: 刺激レベルの目標値Ltに対する特定応答の出現率. 閾値. 強制選択法（2IFC）なら0.75, Yes/No法なら0.5.
+        :param W: Deviation limit. 刺激レベル更新の判断に使用する. 通常1.0~2.0（推奨値1.0）.
+        """
+        self.Xt = 0
+        self.Pt = Pt
+        self.dx = init_dx  # 刺激変化幅.
+        self._min_dx = min_dx  # 最小刺激変化幅. 刺激幅dxが最小刺激幅min_dxとなったときに実験を終了する.
+        self._max_dx = init_dx * 2
+        self._doubled_dx = False
+        self._switched_direction_by_doubled_dx = False
+        self._pre_stimulation_level_direction = self._lower  # lower:False or upper:True
+        self._repeated_num = 1  # 連続で同じ回答が繰り返された回数. 最小値:1,
+        self._consecutive_T = 0  # 一定の刺激レベルXで繰り返された回答数
+        self._consecutive_C = 0  # 一定の刺激レベルXで繰り返された正答数
+        self._W = W  # deviation limit. 1.0 <= W <= 2.0
 
     def update(self, is_correct, X: int) -> int:
-        self.T += 1
-        if is_correct:
-            self.C += 1
+        """(a)刺激レベルを変える時期を判定し, 変化後の刺激レベルを返す.
 
-        W = self.W
-        I = self.Pt * self.T - self.C
+        :param is_correct: 刺激レベル.被験者の回答が正答か否か.
+        :param X: 刺激レベル.
+        :return: 更新後の刺激レベル.
+        """
+        self._consecutive_T += 1
+        if is_correct:
+            self._consecutive_C += 1
+
+        W = self._W
+        I = self.Pt * self._consecutive_T - self._consecutive_C
 
         if np.abs(I) < W:
             self.Xt = X
@@ -46,37 +88,149 @@ class PEST:
         else:
             raise ArithmeticError("Unknown arithmetic error occurred")
 
-        self.find_dx(direction)
-        self.pre_stimulation_level_direction = direction
-        self.Xt = X + self.dx
+        self._consecutive_T = 0
+        self._consecutive_C = 0
+        self._find_dx(direction)
+        self._pre_stimulation_level_direction = direction
+
+        if self.dx > self._max_dx:
+            self.dx = self._max_dx
+
+        if direction == self._lower:
+            self.Xt = X - self.dx
+        else:
+            self.Xt = X + self.dx
         return self.Xt
 
-    def find_dx(self, stimulation_level_direction: bool):
+    def _find_dx(self, stimulation_level_direction: bool):
+        """(b)刺激レベルの変化規則に乗っ取って, 刺激レベルの変化幅を更新する.
+
+        津村尚志 "最近の聴覚心理実験における新しい測定法"（1984） の手順を参考にしている.
+
+        :param stimulation_level_direction: 刺激レベルの変化方向.
+        """
         # (1) 前回との刺激レベルの変化方向を比較
-        if stimulation_level_direction != self.pre_stimulation_level_direction:
-            if self.doubled_dx:
-                self.switched_direction_by_doubled_dx = True
-            self.repeated_num = 1
+        if stimulation_level_direction != self._pre_stimulation_level_direction:
+            if self._doubled_dx:
+                self._switched_direction_by_doubled_dx = True
+            self._repeated_num = 1
             self.dx = int(self.dx / 2)
 
         else:
-            self.repeated_num += 1
+            self._repeated_num += 1
             # (2)
-            if self.repeated_num == 2:
+            if self._repeated_num == 2:
                 # self.dx = self.dx  # プログラム的に無駄なのでコメントアウト
-                self.doubled_dx = False
+                self._doubled_dx = False
             # (3)
-            elif self.repeated_num >= 4:
-                self.dx = 2 * self.repeated_num
-                self.doubled_dx = True
+            elif self._repeated_num >= 4:
+                self.dx = 2 * self._repeated_num
+                self._doubled_dx = True
             # (4)
-            elif self.repeated_num == 3:
-                if self.switched_direction_by_doubled_dx:
+            elif self._repeated_num == 3:
+                if self._switched_direction_by_doubled_dx:
                     # self.dx = self.dx  # プログラム的に無駄なのでコメントアウト
-                    self.doubled_dx = False
+                    self._doubled_dx = False
                 else:
-                    self.dx = 2 * self.repeated_num
-                    self.doubled_dx = False
+                    self.dx = 2 * self._repeated_num
+                    self._doubled_dx = False
 
     def has_ended(self) -> bool:
-        return self.dx == self.min_dx
+        """終了判定. 刺激レベルの変化幅が最小となったとき終了
+        """
+        return self.dx == self._min_dx
+
+
+# -------------------------- Example -------------------------- #
+
+
+def example():
+    """刺激レベルの閾値を推定する例
+
+    心理測定関数PFの閾値Ptに対する刺激レベルの閾値Xtを推定する.
+    """
+
+    def Z(X: float, M: float, S: float) -> float:
+        return (X - M) / S
+
+    def PF(X: float, M: float, S: float, a: float, b: float) -> float:
+        """心理測定関数（ロジスティックス曲線と仮定）"""
+        sigmoid_range = 34.538776394910684
+        z = Z(X, M, S)
+        # オーバーフローを避ける
+        if z <= -sigmoid_range:
+            return 1e-15
+        if z >= sigmoid_range:
+            return 1.0 - 1e-15
+        pf = a / (1 + np.exp(-z)) + b
+        return pf
+
+    def PF_inv(P: float, M: float, S: float, a: float, b: float) -> float:
+        """心理測定関数の逆関数"""
+        pf_inv = M - S * np.log(a / (P-b) - 1)
+        return pf_inv
+
+    # 刺激レベルの対象範囲
+    mock_x = list(range(1, 50))
+
+    # 初期刺激レベル
+    X = 50
+    # 刺激レベルの変化を記録するためのリスト
+    X_list = [X]
+
+    # 心理測定関数のパラメータの真値（強制選択法, 2IFC）
+    true_M = 20
+    true_S = 5
+    true_a = 1/2  # 傾き1/2
+    true_b = 1/2  # バイアス1/2
+    true_Pt = 0.75
+
+    # PEST法のインスタンス生成
+    pest = PEST(init_dx=16, min_dx=1, Pt=true_Pt, W=1.0)
+
+    # 試行回数T
+    T = 0
+    # 実験開始
+    print("実験開始")
+    while True:
+        T += 1
+        # 被験者の回答の正誤
+        is_correct = np.random.rand() < PF(X, true_M, true_S, true_a, true_b)
+
+        # 刺激レベルの更新
+        X = pest.update(is_correct, X)
+        X_list.append(X)
+
+        # 実験終了判定
+        if pest.has_ended():
+            print("実験終了")
+            break
+
+    # 推定結果の出力
+    true_Xt = PF_inv(true_Pt, true_M, true_S, true_a, true_b)
+    print(f"{T}回目の回答で実験が終了しました.")
+    print(f"刺激レベルの閾値. 真値: {true_Xt}, 推定値: {X}")
+
+    # 心理測定関数の閾値をプロット
+    y = [PF(x_tmp, true_M, true_S, true_a, true_b) for x_tmp in mock_x]
+    plt.plot(mock_x, y, color="red", label=f"True PF")
+    plt.hlines(y=true_Pt, xmin=0, xmax=true_Xt, linestyles="--")
+    plt.vlines(x=true_Xt, ymin=1/2, ymax=true_Pt, linestyles="--", label="True threshold")
+    plt.plot([X], [true_Pt], "o", color="orange", label="Estimated threshold")
+    plt.xlabel("Stimulation level X")
+    plt.ylabel("PF(X)")
+    plt.title("Psychometric Function PF")
+    plt.legend()
+    plt.show()
+
+    # 刺激レベルの軌跡
+    plt.plot(list(range(1, T + 2)), X_list, "o-")
+    plt.hlines(y=true_Xt, xmin=0, xmax=T+2, linestyles="--")
+    plt.xlabel("Numbers of trials T")
+    plt.ylabel("Stimulation level X")
+    plt.title("Path of stimulation level")
+    plt.show()
+
+
+if __name__ == '__main__':
+    example()
