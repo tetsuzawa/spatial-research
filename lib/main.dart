e3df9ed2003c63bb13ce8f0c71cf3d5bb8f6ddcf
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-05-14 19:26:22 +0900

    Change: circle buttonsを縁無しにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,184 +1,185 @@
-import 'dart:math';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
-import 'package:flutter_circular_slider/flutter_circular_slider.dart';
-
-void main() => runApp(MyApp());
+void main() => runApp(MyApp()); /*1*/
 
 class MyApp extends StatelessWidget {
+  /*2*/
   @override
   Widget build(BuildContext context) {
+    /*3*/
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      /*4*/
+      title: 'Welcome to Flutter',
+      theme: new ThemeData.light().copyWith(
+          //TODO 色のカスタマイズ
+          ),
+      home: Scaffold(
+        /*5*/
+        appBar: AppBar(
+          /*6*/
+          title: Text('Welcome to Flutter'),
+        ),
+        body: AngleSender(),
       ),
-      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-          child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('images/background_morning.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              child: AngleSenderPage()),
-        ));
-  }
-}
+class _AngleSenderState extends State<AngleSender> {
+  bool _rotation = true;
+  int startPosition = 0;
+  int endPosition = 0;
 
-class AngleSenderPage extends StatefulWidget {
-  @override
-  _AngleSenderPageState createState() => _AngleSenderPageState();
-}
-
-class _AngleSenderPageState extends State<AngleSenderPage> {
-  final baseColor = Color.fromRGBO(255, 255, 255, 0.3);
-
-
-//  int initAngle;
-//  int endAngle;
-
-  int initAngle;
-  int endAngle;
-
-  @override
-  void initState() {
-    super.initState();
-    _shuffle();
-  }
-
-  void _shuffle() {
+  void _handlePressRotationButton() {
     setState(() {
-//      initAngle = _generateRandomTime();
-//      endAngle = _generateRandomTime();
-//      inBedTime = initAngle;
-//      outBedTime = endAngle;
-      initAngle = 0;
-      endAngle = 2;
+      _rotation = !_rotation;
     });
   }
 
-  void _updateLabels(int init, int end, int laps) {
+  void _handlePressCircleElementsButton() {
     setState(() {
-      initAngle = init;
-      endAngle = end;
+      //TODO 最初にボタンが押されたらstartをセット
+      //TODO 二回目ならendをセット
+      //TODO pathを演算する関数を呼び出し
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: [
-        Text(
-          'How long did you stay in bed?',
-          style: TextStyle(color: Colors.white),
+  List<Positioned> createCircleElementsButton(int numElements, double radius) {
+    // リストの初期化
+    var circleElements = new List<Positioned>();
+    // 要素数から角度を計算
+    var angle = 360.0 / numElements;
+    // ボタンの直径
+    var elemRadius = 30.0;
+
+    for (int i = 0; i < numElements; i++) {
+      // 角度と座標を計算
+      var deg = angle * i;
+      var x = math.cos(deg * math.pi / 180 - math.pi) * (radius - elemRadius) +
+          radius;
+      var y = -math.sin(deg * math.pi / 180 - math.pi) * (radius - elemRadius) +
+          radius;
+
+      circleElements.add(
+        Positioned(
+          top: x - elemRadius,
+          left: y - elemRadius,
+          width: elemRadius * 2,
+          height: elemRadius * 2,
+          child: FlatButton(
+            child: Text(deg.toInt().toString()),
+//            child: Text(
+//              "b",
+//              style: TextStyle(fontSize: 5),
+//              textAlign: TextAlign.center,
+//            ),
+
+            onPressed: _handlePressCircleElementsButton,
+            shape: CircleBorder(),
+          ),
         ),
-        Column(
+      );
+    }
+    return circleElements;
+  }
+
+  Widget build(BuildContext context) {
+    const circleElementsFieldWidth = 800.0;
+    return Center(
+      child: Center(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _formatRotation(initAngle, endAngle),
+          children: <Widget>[
+            /* ----------------- Circle Elements -----------------*/
+            Stack(
+              children: <Widget>[
+                Container(
+                  width: circleElementsFieldWidth,
+                  height: circleElementsFieldWidth,
+                  child: Text('word'),
+                  alignment: Alignment.center,
+                ),
+                ...createCircleElementsButton(36, circleElementsFieldWidth / 2)
+              ],
+            ),
+            /* ----------------- Circle Elements -----------------*/
+            /* ----------------- Rotation Button -----------------*/
+            RaisedButton.icon(
+              icon: Icon(
+                _rotation ? Icons.autorenew : Icons.sync,
+                size: 30.0,
+              ),
+              label: Text("Rotation"),
+              color: Theme.of(context).primaryColor,
+              textColor: Theme.of(context).primaryTextTheme.bodyText1.color,
+              shape: StadiumBorder(),
+              onPressed: _handlePressRotationButton,
+            ),
+            /* ----------------- Rotation Button -----------------*/
           ],
         ),
-        DoubleCircularSlider(
-          36,
-//          initAngle,
-//          endAngle,
-          initAngle,
-          endAngle,
-          height: 260.0,
-          width: 260.0,
-          primarySectors: 18,
-          secondarySectors: 36,
-          baseColor: Color.fromRGBO(255, 255, 255, 0.1),
-          selectionColor: baseColor,
-          handlerColor: Colors.white,
-          handlerOutterRadius: 12.0,
-          onSelectionChange: _updateLabels,
-          sliderStrokeWidth: 12.0,
-          child: Padding(
-            padding: const EdgeInsets.all(42.0),
-            child: Center(
-                child: Text('${_formatMovedAngle(initAngle, endAngle)}',
-                    style: TextStyle(fontSize: 36.0, color: Colors.white))),
-          ),
-        ),
-        Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          _formatAnglePosition('START', initAngle),
-          _formatAnglePosition('END', endAngle),
-        ]),
-        FlatButton(
-          child: Text('S H U F F L E'),
-          color: baseColor,
-          textColor: Colors.white,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(50.0),
-          ),
-          onPressed: _shuffle,
-        ),
-      ],
+      ),
     );
   }
-
-  Widget _formatAnglePosition(String pre, int time) {
-    return Column(
-      children: [
-        Text(pre, style: TextStyle(color: baseColor)),
-        Text('ANGLE', style: TextStyle(color: baseColor)),
-        Text(
-          '${_formatAngle(time)}',
-          style: TextStyle(color: Colors.white),
-        )
-      ],
-    );
-  }
-
-  String _formatAngle(int angle) {
-    if (angle == 0 || angle == null) {
-      return '0';
-    }
-//    var hours = angle ~/ 12;
-//    var minutes = (angle % 12) * 5;
-    return '${angle * 10} deg';
-  }
-
-  String _formatMovedAngle(int init, int end) {
-//    var sleepTime = end > init ? end - init : 36 - init + end;
-//    var hours = sleepTime ~/ 12;
-//    var minutes = (sleepTime % 12) * 5;
-    var movedAngle = (end - init).abs() * 10;
-    return '${movedAngle} deg';
-  }
-
-  Widget _formatRotation(int init, int end) {
-    return Column(
-      children: <Widget>[
-        _formatRotationIcon(init, end),
-        Icon(
-          Icons.undo,
-          color: Colors.white,
-          size: 48.0,
-        ),
-      ],
-    );
-  }
-
-  Icon _formatRotationIcon(int init, int end) {
-    return Icon(
-        Icons.redo,
-        color: Colors.white,
-        size: 48.0,
-      );
-  }
-
-  int _generateRandomTime() => Random().nextInt(36);
 }
+
+class AngleSender extends StatefulWidget {
+  @override
+  _AngleSenderState createState() => _AngleSenderState();
+}
+
+/*
+
+class _RotationButtonState extends State<RotationButton> {
+  bool rotation = true;
+
+  void _handlePress() {
+    setState(() {
+      rotation = !rotation;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handlePress,
+      child: Center(
+        child: RaisedButton.icon(
+          icon: Icon(
+            rotation ? Icons.autorenew : Icons.sync,
+            size: 30.0,
+            color: Theme.of(context).backgroundColor,
+          ),
+          label: Text("Rotation"),
+          color: Theme.of(context).primaryColor,
+          textColor: Theme.of(context).backgroundColor,
+          shape: StadiumBorder(),
+          onPressed: _handlePress,
+        ),
+      ),
+    );
+  }
+}
+
+class RotationButton extends StatefulWidget {
+  @override
+  _RotationButtonState createState() => _RotationButtonState();
+}
+*/
+
+/*
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+
+}
+ */
