7df3ddd5fa6f9a0f481e140314c84a33acbea9d6
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-05-16 11:44:05 +0900

    Change: ディレクトリを移動

---
+++ tools/flutter-angle-sender/client/android/app/src/main/kotlin/com/example/angle_sender/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.angle_sender
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tools/flutter-angle-sender/server_example/app.py
@@ -0,0 +1,42 @@
+import time
+import random, string
+
+from flask import Flask, jsonify, request
+
+
+def randomname(n):
+    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
+    return ''.join(randlst)
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return jsonify({'message': 'Hello World!'})
+
+
+@app.route('/', methods=['POST'])
+def angle_reciever():
+    time.sleep(1)  # 何らかの処理
+    json = request.get_json()
+    print("start degree:", json['start_degree'])
+    print("end degree:", json['end_degree'])
+    print("rotation:", json['rotation'])
+    return jsonify({'message': 'OK'})
+
+
+@app.route('/ping')
+def ping():
+    return jsonify({'message': 'OK'})
+
+
+@app.route('/words')
+def random_words():
+    time.sleep(1)
+    return jsonify({'message': randomname(5)})
+
+
+if __name__ == "__main__":
+    app.run(port=8888)
