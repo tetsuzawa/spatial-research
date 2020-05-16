e92120f443819bf849bfeeb251a1072ebc56f54e
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-05-16 11:23:44 +0900

    Fix: testのインポートパスを修正

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:angle_sender/main.dart_old';
+import 'package:angle_sender/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
