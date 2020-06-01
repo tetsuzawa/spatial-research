53ff20b83d77e7f105cd89f17a857bb409a1ac29
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-06-01 17:16:36 +0900

    Add: dxxのライブラリを追加

---
+++ modules/dxx/filelib.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+# ##################################################
+# 研究室で使用している音声ファイル (.DXX 形式)を扱うライブラリ
+#
+# 作成者:瀧澤哲
+# 作成年:2020
+# ##################################################
+
+import os
+
+import numpy as np
+
+exts = [".DSA", ".DFA", ".DDA", ".DSB", ".DFB", ".DDB"]
+dtypes = [np.int16, np.float32, np.float64, np.int16, np.float32, np.float64]
+format_specifiers = ["%d", "%e", "%e"]
+
+
+class BadDataStyleError(Exception):
+    """ファイル形式が適切でないことを知らせる例外クラス"""
+    pass
+
+
+def style(name: str):
+    """ファイルの拡張子を確認する関数。拡張子が不適切であれば例外を発生させる。"""
+    name_ext = os.path.splitext(name)[1]
+
+    if not name_ext in exts:
+        raise BadDataStyleError(f"ファイルの拡張子が不適切です。got: {name_ext}, want: {exts}")
+
+    return exts.index(name_ext)
+
+
+def len_file(name: str) -> int:
+    """データの長さを確認する関数
+
+    example:
+        num_sample = len_file("example.DSB")
+    """
+    data = read_DXX_file(name)
+    return len(data)
+
+
+def read_DXX_file(name: str) -> np.ndarray:
+    """.DXXファイルを読み込む関数
+
+    example:
+        data = read_DXX_file("example.DSB")
+    """
+
+    index = style(name)
+
+    # DXAファイル（アスキー文字列）
+    if index < 3:
+        with open(name, "r") as f:
+            data = np.fromfile(f, dtypes[index], -1)
+        return data
+
+    # DXBファイル（バイナリ）
+    else:
+        with open(name, "rb") as f:
+            data = np.fromfile(f, dtypes[index], -1)
+        return data
+
+
+def write_DXX_file(name: str, data: np.ndarray):
+    """.DXXファイルを書き込む関数
+
+    example:
+        write_DXX_file("example.DDB", data)
+    """
+
+    index = style(name)
+
+    if data.dtype != dtypes[index]:
+        raise BadDataStyleError(f"データの型が拡張子と一致していません。got: {data.dtype}, want: {dtypes[index]}")
+
+    # DXAファイル（アスキー文字列）
+    if index < 3:
+        # カンマ区切りで保存
+        data.tofile(name, sep="\n", format=format_specifiers[index])
+
+    # DXBファイル（バイナリ）
+    else:
+        data.tofile(name)
