c973211e59cd36b1eda7a031f3dc6f1899e9b79f
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-07-02 16:15:25 +0900

    Add: LRのプロットの関数を追加

---
+++ plot_TF_LR/plot_TF_LR.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ##################################################
+# .DXXファイルのLR比較プロット
+#
+# Tetsu Takizawa (tt20805@tomakomai.kosen-ac.jp)
+# 2020
+# ##################################################
+
+import argparse
+import signal
+import matplotlib.pyplot as plt
+
+import dxx
+
+signal.signal(signal.SIGINT, signal.SIG_DFL)
+
+def main():
+    desc = """
+    伝達関数のLRの波形を比較する。(.DSA, .DFA, .DDA, .DSB, .DFB, .DDB)形式のみ対応。
+    読み込む伝達関数を引数で指定して読み込む。
+    """
+    # --------------- 引数の処理 -------------- #
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("L", help="読み込む伝達関数のパス (L)。例: /path/to/SUBJECTS/NAME/SLTF_0_L.DDB")
+    parser.add_argument("R", help="読み込む伝達関数のパス (R)。例: /path/to/SUBJECTS/NAME/SLTF_0_R.DDB")
+    args = parser.parse_args()
+    file_L = args.L
+    file_R = args.R
+    # --------------- 引数の処理 -------------- #
+
+    data_L = dxx.read_DXX_file(file_L)
+    data_R = dxx.read_DXX_file(file_R)
+
+    plt.plot(data_L, alpha=0.5, label="L")
+    plt.plot(data_R, alpha=0.5, label="R")
+    plt.xlabel("Sample")
+    plt.ylabel("Amplitude")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
