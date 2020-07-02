89b0a458d1b0f596b8644d95db3b735a96127b10
audio <audio@maudio6.el.tomakomai-ct.ac.jp>
2020-07-02 16:41:11 +0900

    Add: sample数を指定可能にした

--- tools/plot_TF_LR/plot_TF_LR.py
+++ tools/plot_TF_LR/plot_TF_LR.py
@@ -25,16 +25,20 @@ def main():
     parser = argparse.ArgumentParser(description=desc)
     parser.add_argument("L", help="読み込む伝達関数のパス (L)。例: /path/to/SUBJECTS/NAME/SLTF_0_L.DDB")
     parser.add_argument("R", help="読み込む伝達関数のパス (R)。例: /path/to/SUBJECTS/NAME/SLTF_0_R.DDB")
+    parser.add_argument("--sample", help="プロットする伝達関数のサンプル数 例: 2048", type=int)
     args = parser.parse_args()
     file_L = args.L
     file_R = args.R
+    sample = args.sample
     # --------------- 引数の処理 -------------- #
 
     data_L = dxx.read_DXX_file(file_L)
     data_R = dxx.read_DXX_file(file_R)
+    print("Lの信号長:", len(data_L))
+    print("Rの信号長:", len(data_R))
 
-    plt.plot(data_L, alpha=0.5, label="L")
-    plt.plot(data_R, alpha=0.5, label="R")
+    plt.plot(data_L[:sample], alpha=0.5, label="L")
+    plt.plot(data_R[:sample], alpha=0.5, label="R")
     plt.xlabel("Sample")
     plt.ylabel("Amplitude")
     plt.legend()
