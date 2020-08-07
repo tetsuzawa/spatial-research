fca3eae4a02aae142dbe050ab45de5ea56a94875
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-08-07 09:12:36 +0900

    Fix: echo "" のバグを修正

--- localization_test_move_judge_ts/make_testsignal/make_testsignal_move_judge90_msdv.sh
+++ localization_test_move_judge_ts/make_testsignal/make_testsignal_move_judge90_msdv.sh
@@ -67,7 +67,7 @@ echo "      Adjusting max sound pressure ...                             "
 echo "###################################################################"
 echo
 
-echo "" >| input_files/input_file_move_judge.dat
+printf "" >| input_files/input_file_move_judge.dat
 for move_width in ${move_width_list[@]}; do
   for move_velocity in ${move_velocity_list[@]}; do
     for end_angle in ${end_angle_list[@]}; do
